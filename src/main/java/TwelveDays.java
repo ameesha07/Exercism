2a094a97f1d648282bc03af2e7911ffc019fdeeb
Ameesha Rani <ameeshasingh55@gmail.com>
2022-07-24 19:12:35 +0530

    twelve-days.java

--- src/main/java/TwelveDays.java
+++ src/main/java/TwelveDays.java
@@ -47,7 +47,7 @@ class TwelveDays {
                 "and a Partridge in a Pear Tree.\n");
         put(11, "On the eleventh day of Christmas my true love gave to me: " +
                 "eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, " +
-                "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, " +
+                "seven Swans-a-Swimming, six Geese-a-Laying, gfive Gold Rings, four Calling Birds, " +
                 "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n");
         put(12, "On the twelfth day of Christmas my true love gave to me: " +
                 "twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, " +
